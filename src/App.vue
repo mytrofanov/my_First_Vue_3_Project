3904d594286fe20421d84cee479bb2068bfd9f9b
mytrofanov <mytrofanovmaxym@gmail.com>
2023-07-20 12:25:14 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>Likes: {{likes}}</div>
+    <div>Dislikes: {{dislikes}}</div>
+    <button @click="adLike">Like</button>
+    <button @click="adDisLike">Dislike</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    adLike () {
+      this.likes +=1
+    },
+    adDisLike () {
+      this.dislikes +=1
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
